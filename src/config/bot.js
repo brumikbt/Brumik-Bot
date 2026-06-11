d995be9517563e4707d850993803489b6ada0c40
brumikbt <petra.skrakova@gmail.com>
2026-06-11 04:00:47 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "[34 members] 👑| Brumiková Crew",
+        name: "[34/100 members] 👑| Brumiková Crew",
         // Activity type number (0 = Playing).
         type: 0, 
       },
